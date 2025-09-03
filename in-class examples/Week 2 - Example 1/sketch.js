2489ab6ed2e107bc286707e5bb08b7cdd242ee1c
Ofer-Shouval <os436@nyu.edu>
2025-09-03 11:11:45 -0400

    Added first example sketch

---
+++ in-class examples/Week 2 - Example 1/sketch.js
@@ -0,0 +1,19 @@
+function setup(){
+
+    createCanvas(600, 400);
+    background(0);
+    rectMode(CENTER);
+
+    fill(255,0,0)
+    rect(150,50,80,100,5,50,10,25)
+
+    fill(255, 200, 0)
+    rect(250,200,80,100)
+
+}
+
+function draw(){
+
+
+
+}
