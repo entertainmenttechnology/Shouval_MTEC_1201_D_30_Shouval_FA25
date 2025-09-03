b7eee8ccc36ce9d9f8f2b23e00645e78fda4de3a
Ofer-Shouval <os436@nyu.edu>
2025-09-03 12:03:20 -0400

    Added in class examples

---
+++ in-class examples/Week 2 - Example 2/sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+
+    createCanvas(windowWidth, windowHeight)
+    background(100);
+    
+    noStroke();
+    stroke(0,0,255)
+
+}
+
+function draw(){
+
+    
+    line(0,0, width, height )
+
+
+
+}
---
+++ in-class examples/Week 2 - Example 3/sketch.js
@@ -0,0 +1,25 @@
+function setup(){
+
+    createCanvas(windowWidth, windowHeight)
+    // background(100);
+    
+    // noStroke();
+    // strokeWeight(5)
+  //frameRate(2)
+
+}
+
+function draw(){
+
+    background(100,50);
+
+
+    stroke(0,0,255)
+    line(0,0, mouseX, mouseY )
+    noStroke();
+    ellipse(mouseX, mouseY, 50,20)
+
+    console.log(mouseX, mouseY);
+    
+
+}
